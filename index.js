fb7e85aa27d7344ecdf9f6ad3f9af42ed8f240f4
AASHUTOSH PRASHANT <60566163+anshobrii@users.noreply.github.com>
2022-02-19 20:34:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    $("#list-items").html(localStorage.getItem("listItems"));
+
+    $(".add-items").submit(function(event){
+        event.preventDefault();
+
+        var item = $("#todo-list-item").val();
+
+        if (item) {
+        $("#list-items").append("<li><input class = 'checkbox' type='checkbox'/>"+ item +  "<a class='remove'>x</a><hr></li>")
+        localStorage.setItem("listItems",$("#list-items").html());
+        
+        $("#todo-list-item").val("");
+        } 
+
+    });
+
+    document.querySelector("#btnLearn").addEventListener("click", event => {
+        location.href = "https://www.linkedin.com/in/anshobrii/";
+    })
+
+    
+    //  $(".checkbox").change(function(){
+    //      console.log("checkbox checked!");
+    //  })
+
+    $(document).on("change", ".checkbox", function() {
+        if($(this).attr("checked")){ 
+                $(this).removeAttr("checked");
+        } else{
+             $(this).attr("checked","checked");
+        }
+       
+        $(this).parent().toggleClass("completed");
+        localStorage.setItem("listItems",$("#list-items").html());
+
+    });
+
+
+    $(document).on("click",".remove", function(){
+        $(this).parent().remove();
+        localStorage.setItem("listItems",$("#list-items").html());
+    });
+
+});
+//Add animation if someone add something like dance song or any activity then that relable 
+// animation pop to the screen
+//Add fadein and fadeout to the todo item when deleted or added 
